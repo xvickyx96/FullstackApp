69235c387fba1f4e6828b97a293c7a7c12d0e593
xvickyx96 <96viksin@gafe.molndal.se>
2025-01-16 12:01:54 +0100

    update

--- Fullstack-app/src/main/java/com/example/vikram/Fullstack/app/controller/UserController.java
+++ Fullstack-app/src/main/java/com/example/vikram/Fullstack/app/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.vikram.Fullstack.app.controller;
 
+import com.example.vikram.Fullstack.app.exception.UserNotFoundException;
 import com.example.vikram.Fullstack.app.model.User;
 import com.example.vikram.Fullstack.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -21,6 +21,36 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        try {
+            return userRepository.findById(id)
+                    .orElseThrow(()-> new UserNotFoundException(id));
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping("/user/{id}")
+        User updateUser (@RequestBody User newUser, @PathVariable Long id) throws UserNotFoundException {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) throws UserNotFoundException {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted.";
     }
 
 
